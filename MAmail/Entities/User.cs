95022e87673d92fcf19a00d0d964cb4ba46da22a
Alex-dev02 <ionut02alex@yahoo.ro>
2023-03-23 20:03:32 +0200

    added User entity

---
+++ MAmail/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MAmail.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
