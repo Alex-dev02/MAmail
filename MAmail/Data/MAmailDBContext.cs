9f2bb4f2a5fbb2128560a4a739e2f51d49e8f6ce
Alex-dev02 <ionut02alex@yahoo.ro>
2023-03-23 20:56:19 +0200

    added database context and runned first migration

---
+++ MAmail/Data/MAmailDBContext.cs
@@ -0,0 +1,17 @@
+﻿using MAmail.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MAmail.Data
+{
+    public class MAmailDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Not safe
+            optionsBuilder
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MAmailDB;Trusted_Connection=True;");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
