c79525cbb2f00b2a174f1d1abcd7aa05aafe7ea4
Alex-dev02 <ionut02alex@yahoo.ro>
2023-06-24 22:57:27 +0300

    Email entity

---
+++ MAmail/Entities/Email.cs
@@ -0,0 +1,13 @@
+﻿namespace MAmail.Entities
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int SenderId { get; set; }
+        public User? Sender { get; set; }
+
+    }
+}
