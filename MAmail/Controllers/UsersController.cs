ddf0a1926bf8f87c3940fc973c8495b24bcb0d1a
Alex-dev02 <ionut02alex@yahoo.ro>
2023-06-24 17:16:57 +0300

    configured authentication & authorization

--- MAmail/Controllers/UsersController.cs
+++ MAmail/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using MAmail.Dtos;
 using MAmail.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MAmail.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("/users")]
     public class UsersController : ControllerBase
     {
