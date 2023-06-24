7d602e9a0c0267350c63a3f48dbe9138c7b96834
Alex-dev02 <ionut02alex@yahoo.ro>
2023-06-24 12:55:01 +0300

    authorization service for user registration

---
+++ MAmail/Services/AuthorizationService.cs
@@ -0,0 +1,21 @@
+﻿using MAmail.Dtos;
+using MAmail.Repositories;
+using MAmail.Utils;
+
+namespace MAmail.Services
+{
+    public class AuthorizationService
+    {
+        private AuthorizationRepository _authorizationRepository;
+
+        public AuthorizationService(AuthorizationRepository authorizationRepository)
+        {
+            _authorizationRepository = authorizationRepository;
+        }
+
+        public async Task<RegisterResponse> Register(UserCreateRequestDto user)
+        {
+            return await _authorizationRepository.Register(user);
+        }
+    }
+}
